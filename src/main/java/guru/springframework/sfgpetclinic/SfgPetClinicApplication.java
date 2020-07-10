ce7f3966c732f0636e6ee81c32652abae6dafd2e
jlee1976apple <jlee1976apple@gmail.com>
2020-07-10 18:22:05 -0400

    Initial Project Add.#2

--- src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//This shows up!
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
