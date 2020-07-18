1226ffc1a35f849fc24f702db48caf2a0249e5b8
jlee1976devops <jlee1976apple@gmail.com>
2020-07-18 12:24:11 -0400

    Updated Profiles to use Spring Data JPA Services.  Closes #48

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PersonServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/PersonServiceMap.java
@@ -2,9 +2,13 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Person;
 import guru.springframework.sfgpetclinic.services.CrudService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
+@Profile({"default","map"})
 public class PersonServiceMap extends AbstractMapService<Person, Long> implements CrudService<Person, Long> {
     @Override
     public Person findById(Long Id) {
